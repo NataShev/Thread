55a2a6a5acc464e2806d1d48d46f7f82dc38c59d
Natalya Shevchenko <nsevcenko1988@gmail.com>
2022-03-24 11:33:28 +0300

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -1,39 +1,41 @@
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.*;
 
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
-        Collection<Thread> mainGroup = new ArrayList<>(3);
-        for (int i = 0; i < 3; i++) {
-            mainGroup.add(new MyThread("номер " + (i + 1)));
+    static int counter;
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
+        ExecutorService mainEx = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+        List<MyCallable> tasks = new ArrayList<>();
+        for (int i = 0; i < 4; i++) {
+            MyCallable mc = new MyCallable("поток номер " + (i + 1));
+            tasks.add(mc);
         }
-        for (Thread thread : mainGroup) {
-            thread.start();
-        }
-        Thread.sleep(15_000);
-        for (Thread thread : mainGroup) {
-            thread.interrupt();
-        }
+        List<Future<Integer>> futureTasks = mainEx.invokeAll(tasks);
+        Integer ancwer = mainEx.invokeAny(tasks);
+
+        mainEx.shutdown();
+        System.out.println("Колличество напечатаных сообщений - " + ancwer);
     }
 
-    static class MyThread extends Thread {
+    static class MyCallable extends Thread implements Callable<Integer> {
 
-        public MyThread(String name) {
+        public MyCallable(String name) {
             super(name);
         }
 
         @Override
-        public void run() {
+        public Integer call() throws Exception {
             try {
-                while (!isInterrupted()) {
-                    Thread.sleep(2500);
-                    System.out.println("Всем привет!Я поток " + getName());
-                }
+                System.out.println("Всем привет!Я " + getName());
+                counter++;
+                Thread.sleep(2500);
             } catch (InterruptedException err) {
-                System.out.println("Поток спал, когда его пытались завершить!");
-            } finally {
-                System.out.printf("%s завершен\n", getName());
+                System.out.println("Поток спал, когда его пытались завершить");
             }
+            return counter;
         }
     }
 }
+
